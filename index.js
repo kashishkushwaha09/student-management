5a27ed89a7fcc1e6a5f849581fbe1bae292f5052
kashishkushwaha09 <kashishkushwaha03@gmail.com>
2025-05-16 09:43:06 +0530

    db using sequelize

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express= require('express');
 const app=express();
 const db=require('./utils/db-connection');
 const studentRouter=require('./routes/studentRoutes');
-
+const studentsModel=require('./models/studentModel')
 app.use(express.json());
 app.use('/api/students',studentRouter);
+db.sync({force:true}).then(()=>{
 app.listen(3200,()=>{
     console.log("server is listening on port 3200");
+})
+})
+.catch((err)=>{
+    console.log(err);
 })
--- utils/db-connection.js
+++ utils/db-connection.js
@@ -1,33 +1,53 @@
-// student-management
-const mysql=require('mysql2');
-
-const connection=mysql.createConnection({
+const {Sequelize}=require('sequelize');
+const sequelize=new Sequelize('student-management','root','Khush@123',{
     host:'localhost',
-    user:'root',
-    password:'Khush@123',
-    database:'student-management'
-})
-connection.connect((err)=>{
-    if(err){
-        console.log(err);
-        return;
+    dialect:'mysql'
+});
+(
+    async()=>{
+try {
+    await sequelize.authenticate();
+    console.log("connection to db has been created");
+} catch (error) {
+    console.log(error);
+}
     }
-    console.log("connection has been created");
-    const students=`create table IF NOT EXISTS Students(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
-    email VARCHAR(255) UNIQUE,
-    age INT
-    )`
+)();
+
+module.exports=sequelize;
+
+
+
+// student-management
+// const mysql=require('mysql2');
+
+// const connection=mysql.createConnection({
+//     host:'localhost',
+//     user:'root',
+//     password:'Khush@123',
+//     database:'student-management'
+// })
+// connection.connect((err)=>{
+//     if(err){
+//         console.log(err);
+//         return;
+//     }
+//     console.log("connection has been created");
+//     const students=`create table IF NOT EXISTS Students(
+//     id INT AUTO_INCREMENT PRIMARY KEY,
+//     name VARCHAR(255),
+//     email VARCHAR(255) UNIQUE,
+//     age INT
+//     )`
     
-    connection.execute(students,(err)=>{
-        if(err){
-            console.log(err);   
-            connection.end();
-            return;
-        }
-        console.log('students has been created');
-    })
+//     connection.execute(students,(err)=>{
+//         if(err){
+//             console.log(err);   
+//             connection.end();
+//             return;
+//         }
+//         console.log('students has been created');
+//     })
   
-})
-module.exports=connection;
+// })
+// module.exports=connection;
