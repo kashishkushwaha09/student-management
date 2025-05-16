32c185ff43edb2618a4bdb1dfae5cc3adb63f5b6
kashishkushwaha09 <kashishkushwaha03@gmail.com>
2025-05-16 10:52:40 +0530

    delete functionality

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -45,42 +45,25 @@ const updateEntries=async (req,res)=>{
         res.status(500).send(err.message);
  }
     
-    // let updateQuery,fields;
-    // if(name && email){
-    // updateQuery=`UPDATE students set name=?,email=? WHERE id=?`;
-    //  fields=[name,email,id];
-    // }
-    // db.execute(updateQuery,fields,(err,result)=>{
-    //     if(err){
-    //         console.log(err.message);
-    //     res.status(500).send(err.message);
-    //     db.end();
-    //     return;
-    //     }
-    //     if(result.affectedRows===0){
-    //         res.status(404).send("student not found");
-    //         return;
-    //     }
-    //     res.status(200).send("student has been updated");
-    // })
 }
-const deleteEntry=(req,res)=>{
-    const {id}=req.params;
-    const deleteQuery=`DELETE FROM students WHERE id=?`;
-    db.execute(deleteQuery,[id],(err,results)=>{
-    if(err){
-        console.log(err.message);
+const deleteEntry=async (req,res)=>{
+    try {
+        const {id}=req.params; 
+        const student=await Student.destroy({
+            where:{
+                id:id
+            }
+        })
+        if(!student){
+            res.status(404).send('User not found');
+        }
+        res.status(200).send(`student with id ${id} is deleted`);
+    } catch (err) {
+            console.log(err);
         res.status(500).send(err.message);
-        db.end();
-        return;
     }
-    console.log("student has been deleted");
-    if(results.affectedRows===0){
-        res.status(404).send("student not found");
-        return;
-    }
-    res.status(200).send(`student with id ${id} is deleted`);
-    })
+   
+   
 }
 
 module.exports={retrieveEntries,addEntries,updateEntries,deleteEntry};
